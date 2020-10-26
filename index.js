5d2d110f29127e78fbee6ce7d683e2fd38f85047
RicardormDev <romr020606@gmail.com>
2020-10-26 00:39:27 -0600

    idnexi

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
-});
-
-io.on('connection', (socket) => {
-    socket.on('chat message', (message) => {
-        console.log('message: ' + message);
-        io.emit('chat message', message);
-    });
 });
 
 http.listen(process.env.PORT || 3500, () => {
